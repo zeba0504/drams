32550d3bcc295a1efe8b1f37b620ef980b565e99
Zeba <saidzodazebida@gmail.com>
2025-12-04 02:00:50 +0300

    seventh

--- app/src/main/java/com/example/andromonth/DayScreen.kt
+++ app/src/main/java/com/example/andromonth/DayScreen.kt
@@ -1,10 +1,63 @@
 package com.example.andromonth
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import com.example.andromonth.model.Day
 
+@Composable
+fun DayCardItem(day: Day, index: Int, modifier: Modifier = Modifier) {
+    var expand by remember {
+        mutableStateOf(false)
+    }
+    val color by animateColorAsState(
+        targetValue = if (expand) MaterialTheme.colorScheme.primaryContainer else MaterialTheme.colorScheme.secondaryContainer
+    )
+    Card(
+        modifier = modifier
+            .padding(8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+    ) {
+
+        Column(
+            Modifier
+                .background(color = color)
+                .animateContentSize(
+                    animationSpec = spring(
+                        stiffness = Spring.StiffnessMediumLow,
+                        dampingRatio = Spring.DampingRatioLowBouncy
+                    )
+                )
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+
+            }
+        }
+    }
+}
 @Composable
 fun DayScreenList(days: List<Day>, modifier: Modifier = Modifier) {
     LazyColumn(modifier = modifier) {}
--- app/src/main/java/com/example/andromonth/MainActivity.kt
+++ app/src/main/java/com/example/andromonth/MainActivity.kt
@@ -4,16 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.andromonth.model.DayRepo
 import com.example.andromonth.ui.theme.AndroMonthTheme
 
@@ -45,7 +53,25 @@ fun AndroMonthApp() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AndroMonthTopBar() {}
+fun AndroMonthTopBar() {
+    CenterAlignedTopAppBar(title = {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = stringResource(id = R.string.days_30),
+                style = MaterialTheme.typography.displayLarge
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.ic_android),
+                contentDescription = "android",
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .size(48.dp),
+                tint = MaterialTheme.colorScheme.primary
+            )
+        }
+    })
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
